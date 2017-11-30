bf1306c2e89e127e9932cce31b6e44509a290952
Yudhishthir Kaushik <yudhishthirbpl@gmail.com>
2017-11-30 15:27:01 +0000

    Update carService.js
    fixed IP address

--- src/main/resources/public/carService.js
+++ src/main/resources/public/carService.js
@@ -5,14 +5,14 @@ var app = angular.module("CarDashboard", []);
 
 
 app.controller('CarDashboard', function($scope, $http) {
-    $http.get('http://localhost:9989/cars/all').
+    $http.get('http://172.20.10.5:9989/cars/all').
         then(function(response) {
             $scope.cars = response.data;
         });
     
 	$scope.showModelDetails = function (data) {
 		console.log("hello, You have selected " + data + ", for more details");
-		$http.get('http://localhost:9989/cars/'+ data).
+		$http.get('http://172.20.10.5:9989/cars/'+ data).
         then(function(response) {
             $scope.car = response.data;
         });
